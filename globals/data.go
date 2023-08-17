cbcd5e22c34c10769488ca8ebc59eed71a0c11a3
Midas van Veen <midas@midaspro.local>
2023-08-17 16:01:33 +0200

    changed storage

--- globals/data.go
+++ globals/data.go
@@ -1,12 +1,9 @@
 package globals
 
 var (
-	Table = make(map[string][]string, 0)
-)
-
-const (
-	UAS     = "uas"
-	ACCEPTS = "accepts"
-	REFS    = "refs"
-	PROXIES = "proxies"
+	// Table   = make(map[string][]string, 0)
+	UAS     = make([]string, 0)
+	ACCEPTS = make([]string, 0)
+	REFS    = make([]string, 0)
+	PROXIES = make([]string, 0)
 )
